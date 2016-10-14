f5cf16efcfe40b1186c2a21fa6a621e477a88ee4
Jason Gullifer <jason.gullifer@gmail.com>
2016-10-14 15:32:27 -0400

    practicals

---
+++ docs/practicals/practical2a.R
@@ -0,0 +1,22 @@
+##############################################################################
+##
+##					Practical 1: part a
+##
+##############################################################################
+
+##############################################################################
+##	Background:
+##	For practical 2, we will be plotting data from two datasets: the hot or not 
+##	dataset (part a) you worked with in practical 1, and the ANT dataset (part b)
+##	from the ez package. If you completed practical1_extra.R feel free to use
+##	that version of the dataset. Otherwise, you can use the dataset provided by
+##	the package.
+##############################################################################
+
+#######################     HOT OR NOT     ###################################
+
+
+
+
+
+##############################################################################
